ca1052af033311e281df430cf663e1788d231417
EmilianoArias <32575456+Sazakuni7@users.noreply.github.com>
2024-10-27 20:45:34 -0300

    Update Rotacion.cpp
    Se modificó para que no se rote solo 1 vez el sprite, si no que mientras la ventana esté abierta, rote constantemente 1 grado
    
    Note: queda investigar disminuir la velocidad de rotación

--- Actividad 5 Rotacion/Rotacion.cpp
+++ Actividad 5 Rotacion/Rotacion.cpp
@@ -8,19 +8,20 @@ int main() {
 		"Actividad 5 - Rotacion");
 	texture.loadFromFile("cuad_red.png");
 	sprite.setTexture(texture);
-	//InvestiguÈ que el siguiente metodo comentado tambiÈn resuelve la actividad, y consiste en que getGlobalBounds obtiene las dimensiones del sprite despuÈs
-	// de aplicar escalados o transformaciones. Tanto este como el mÈtodo usado son necesarios para fijar el origen del sprite al centro del mismo y asÌ poder rotar
-	// desde un punto m·s conveniente.
+	//Investigu√© que el siguiente metodo comentado tambi√©n resuelve la actividad, y consiste en que getGlobalBounds obtiene las dimensiones del sprite despu√©s
+	// de aplicar escalados o transformaciones. Tanto este como el m√©todo usado son necesarios para fijar el origen del sprite al centro del mismo y as√≠ poder rotar
+	// desde un punto m√°s conveniente.
 	//sf::FloatRect bounds = sprite.getGlobalBounds();
 	//sprite.setOrigin(bounds.width / 2, bounds.height / 2);
 	Vector2u size = sprite.getTexture()->getSize();
 	sprite.setOrigin(size.x / 2.0f, size.y / 2.0f);
 	sprite.setPosition(400, 300);
-	sprite.rotate(60.f);
 	// Loop principal
 	while (App.isOpen())
 	{
 		App.clear();
+		//Mientras ventana est√© abierta, el sprite rotar√° 1 grado
+		sprite.rotate(1.f);
 		App.draw(sprite);
 		App.display();
 	}
